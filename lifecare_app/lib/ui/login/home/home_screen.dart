2da2b1b8ee6b7932081b68db4dc5b310723523fa
vaishnavi9248 <vaishnavi9248@gmail.com>
2023-03-29 00:37:56 +0530

    heartbeat and temperature reactive value update with graph and text completed

--- lifecare_app/lib/ui/login/home/home_screen.dart
+++ lifecare_app/lib/ui/login/home/home_screen.dart
@@ -5,6 +5,9 @@ import 'package:chart_sparkline/chart_sparkline.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
+List<double> heartBeatList = [];
+List<double> temperatureList = [];
+
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
@@ -15,22 +18,32 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   static Random random = Random();
 
-  List<double> heartBeatList = [];
-  List<double> temperatureList = [];
-
   @override
   void initState() {
     generateCountList();
-    addNewValue();
+    updateHeartBeat();
+    updateTemperature();
     super.initState();
   }
 
-  void addNewValue() {
-    Timer.periodic(const Duration(seconds: 1), (timer) {
+  void updateHeartBeat() {
+    Timer.periodic(const Duration(milliseconds: 1500), (timer) {
       Random random = Random();
-      double newHeart = 60 + random.nextDouble() * (100 - 60);
+      double newHeart = double.parse(
+          (60 + random.nextDouble() * (100 - 60)).toStringAsFixed(2));
       heartBeatList.removeAt(0);
       heartBeatList.add(newHeart);
+      setState(() {});
+    });
+  }
+
+  void updateTemperature() {
+    Timer.periodic(const Duration(milliseconds: 2500), (timer) {
+      Random random = Random();
+      double newTempe = double.parse(
+          (36.1 + random.nextDouble() * (37.2 - 36.1)).toStringAsFixed(2));
+      temperatureList.removeAt(0);
+      temperatureList.add(newTempe);
       setState(() {});
     });
   }
@@ -58,32 +71,57 @@ class _HomeScreenState extends State<HomeScreen> {
       body: SafeArea(
         child: Center(
           child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 22.0),
+            padding: const EdgeInsets.only(right: 22.0),
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                SizedBox(
-                  height: 300.0,
-                  child: Sparkline(
-                    data: temperatureList,
-                    enableGridLines: true,
-                    fillMode: FillMode.below,
-                    fillColor: Colors.red.withOpacity(0.5),
-                    gridLineAmount: 15,
-                    max: 37.4,
-                    min: 36,
-                  ),
+                const SizedBox(height: 12.0),
+                const Text(
+                  "HeartBeat",
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 26.0),
                 ),
-                const SizedBox(height: 50),
-                SizedBox(
-                  height: 300.0,
+                const SizedBox(height: 4.0),
+                Text(
+                  "${heartBeatList.last} BPM",
+                  style: const TextStyle(
+                      fontWeight: FontWeight.bold, fontSize: 18.0),
+                ),
+                const SizedBox(height: 8.0),
+                Expanded(
                   child: Sparkline(
                     data: heartBeatList,
                     enableGridLines: true,
-                    fillMode: FillMode.below,
-                    fillColor: Colors.red.withOpacity(0.5),
+                    // fillMode: FillMode.below,
+                    // fillColor: Colors.red.withOpacity(0.5),
+                    min: 60,
+                    max: 100,
+                    gridLineAmount: 9,
                   ),
                 ),
+                const SizedBox(height: 12.0),
+                const Text(
+                  "Temperature",
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 26.0),
+                ),
+                const SizedBox(height: 4.0),
+                Text(
+                  "${temperatureList.last} °C",
+                  style: const TextStyle(
+                      fontWeight: FontWeight.bold, fontSize: 18.0),
+                ),
+                const SizedBox(height: 8.0),
+                Expanded(
+                  child: Sparkline(
+                    data: temperatureList,
+                    enableGridLines: true,
+                    //fillMode: FillMode.below,
+                    //fillColor: Colors.red.withOpacity(0.5),
+                    gridLineAmount: 12,
+                    max: 37.2,
+                    min: 36.1,
+                  ),
+                ),
+                const SizedBox(height: 8.0),
               ],
             ),
           ),
