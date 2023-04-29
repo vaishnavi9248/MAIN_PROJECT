d5334e905e562bcfc6521006667fdbef0dc27b46
vaishnavi9248 <vaishnavi9248@gmail.com>
2023-04-29 10:44:45 +0530

    .gitignore

--- lifecare_server/key.js
+++ lifecare_server/key.js
@@ -2,7 +2,4 @@ module.exports = {
   MONGO_URL:
     "mongodb+srv://lifecare_username:lifecare_password@main.qhy965h.mongodb.net/?retryWrites=true&w=majority",
   SERVER_URL: "http://192.168.43.57:5678/",
-  IMAGE_PATH: __dirname + "../uploads/images",
-  DOCS_PATH: __dirname + "../uploads/docs",
-  VIDEOS_PATH: __dirname + "../uploads/videos",
 };
